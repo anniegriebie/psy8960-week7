033b1a43f1c36e001c6a2f6f1bfedde63b562fce
anniegriebie <GRIEB046@umn.edu>
2023-03-12 12:21:36 -0400

    Data Import and Cleaning

--- R/week7.R
+++ R/week7.R
@@ -1,8 +1,16 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
-
-
+library(lubridate)
+library(GGally)
 
 
 # Data Import and Cleaning
+week7_tbl <- read.csv("../data/week3.csv",  header = TRUE) %>%
+  mutate(timeStart = ymd_hms(timeStart)) %>%
+  mutate(timeEnd = ymd_hms(timeEnd)) %>%
+  mutate(gender = factor(gender, levels = c("F", "M"), labels = c("Female", "Male"))) %>%
+  mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
+  filter(q6 == 1) %>%
+  select(-q6) %>%
+  mutate(timeSpent = difftime(timeEnd, timeStart, units =c("mins")))
