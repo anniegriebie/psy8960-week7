3ee807d32ed0f10bb817ee4f6d3ef54d3ff39aea
anniegriebie <GRIEB046@umn.edu>
2023-03-12 13:26:17 -0400

    Edited order of gender column in order to correctly match Figure 2, creation of Figure 2

--- R/week7.R
+++ R/week7.R
@@ -9,7 +9,7 @@ library(GGally)
 week7_tbl <- read.csv("../data/week3.csv",  header = TRUE) %>%
   mutate(timeStart = ymd_hms(timeStart)) %>%
   mutate(timeEnd = ymd_hms(timeEnd)) %>%
-  mutate(gender = factor(gender, levels = c("F", "M"), labels = c("Female", "Male"))) %>%
+  mutate(gender = factor(gender, levels = c("M", "F"), labels = c("Male",  "Female"))) %>%
   mutate(condition = factor(condition, levels = c("A", "B", "C"), labels = c("Block A", "Block B", "Control"))) %>%
   filter(q6 == 1) %>%
   select(-q6) %>%
@@ -26,4 +26,9 @@ week7_tbl %>%
     geom_point() +
     scale_x_datetime("Date of Experiment")+
     scale_y_continuous("Q1 Score")) %>%
-  ggsave("../figs/fig1.png",., width = 9, height = 5 , units = "in") 
+  ggsave("../figs/fig1.png",., width = 9, height = 5 , units = "in")
+
+(ggplot(week7_tbl, aes(x=q1, y=q2, color=gender)) +
+    geom_jitter() +
+    labs(color = "Participant Gender")) %>%
+  ggsave("../figs/fig2.png",., width = 9, height = 4, units = "in")
