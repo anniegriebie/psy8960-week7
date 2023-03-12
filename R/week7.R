230347b89668c2ff68b0a35b8f2707b55e5a5740
anniegriebie <GRIEB046@umn.edu>
2023-03-12 12:42:37 -0400

    Initial visualization and creation of figure 1

--- R/week7.R
+++ R/week7.R
@@ -14,3 +14,16 @@ week7_tbl <- read.csv("../data/week3.csv",  header = TRUE) %>%
   filter(q6 == 1) %>%
   select(-q6) %>%
   mutate(timeSpent = difftime(timeEnd, timeStart, units =c("mins")))
+
+
+
+
+# Visualization
+week7_tbl %>% 
+  select(starts_with("q")) %>% 
+  ggpairs()
+(ggplot(week7_tbl, aes(x=timeStart, y=q1)) +
+    geom_point() +
+    scale_x_datetime("Date of Experiment")+
+    scale_y_continuous("Q1 Score")) %>%
+  ggsave("../figs/fig1.png",., width = 9, height = 5 , units = "in") 
